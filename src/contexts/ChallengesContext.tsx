ae07ccb538f15a34c40128facd17f279b69f8dc0
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-02-25 13:59:27 -0300

    Navigator Notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 
 import challenges from '../../challenges.json'
 
@@ -34,6 +34,10 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
   const [activeChallenge, setActiveChallenge] = useState(null);
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+  useEffect(() => {
+    Notification.requestPermission();
+  }, [])
   
   function levelUp() {
     setLevel(level + 1);
@@ -44,6 +48,12 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge)
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio 🎉', {
+        body: `Valendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   function resetChallenge() {
